b42ebf8b4dba3b7cc7de04f2afb4e574d11e144b
LeelaSravaniAtmakuri <50482163+LeelaSravaniAtmakuri@users.noreply.github.com>
2019-06-12 11:21:16 +0530

    Updated Lip makeup_CNN

--- Lip makeup_CNN.py
+++ Lip makeup_CNN.py
@@ -40,7 +40,7 @@ img_cols=150
 
 num_channel=3
 
-num_epoch=5
+num_epoch=10
 
 
 
@@ -77,9 +77,6 @@ img_data /= 255
 print (img_data.shape)
 
 
-# Assigning Labels
-
-
 
 # Define the number of classes
 
@@ -92,6 +89,7 @@ num_of_samples = img_data.shape[0]
 labels = np.ones((num_of_samples,),dtype='int64')
 
 
+# Assigning Labels
 
 labels[0:52]=0
 
@@ -102,13 +100,13 @@ names = ['Lip Makeup','No Makeup']
 
 
 
-# convert class labels to on-hot encoding
+# Convert class labels to One-hot encoding
 
 Y = np_utils.to_categorical(labels, num_classes)
 
 
 
-#Shuffle the dataset
+# Shuffle the dataset
 
 x,y = shuffle(img_data,Y, random_state=2)
 
@@ -169,18 +167,18 @@ model.add(Activation('softmax'))
 
 
 
-#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-#model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
+# model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
 
-#model.compile(loss='categorical_crossentropy', optimizer='adadelta',metrics=["accuracy"])
+# model.compile(loss='categorical_crossentropy', optimizer='adadelta',metrics=["accuracy"])
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
 
 
 
 # Viewing model_configuration
 
-'''
+
 
 model.summary()
 
@@ -198,7 +196,7 @@ np.shape(model.layers[0].get_weights()[0])
 
 model.layers[0].trainable
 
-'''
+
 
 
 # Training
@@ -218,17 +216,68 @@ print('Test accuracy:', score[1])
 
 
 
-test_image = X_test[1:2]
+# Testing with an image in testing set
+
+test_image = X_test[2:3]
 
 print (test_image.shape)
+
 print(model.predict(test_image))
 
 print('Predicted:',names[model.predict_classes(test_image)[0]])
 
-res= y_test[0:1]
+res= y_test[2:3]
 #print(res)
 for lis in res:
     for index,item in enumerate(lis):
         if item==1:
             ind = index
 print('Original:',names[ind])
+
+
+
+# Testing with a new image
+'''
+
+test_image = cv2.imread(r'F:\Smart mirror\Lipmakeup test\test7.jpg')
+
+# test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('image', test_image)
+
+face_cascade = cv2.CascadeClassifier(r"F:\Anaconda\Library\etc\haarcascades\haarcascade_frontalface_default.xml")
+
+gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+
+face = face_cascade.detectMultiScale(gray, 1.2, 5)
+
+for (x, y, w, h) in face:
+    test_image = test_image[y:y + h, x:x + w]
+
+test_image = cv2.resize(test_image, (130, 150))
+
+test_image = np.array(test_image)
+
+test_image = test_image.astype('float32')
+
+test_image /= 255
+
+print(test_image.shape)
+
+test_image = np.expand_dims(test_image, axis=0)
+
+print(test_image.shape)
+
+# Predicting the test image
+
+print((model.predict(test_image)))
+
+print(model.predict_classes(test_image))
+
+print('Predicted:', names[model.predict_classes(test_image)[0]])
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+'''
